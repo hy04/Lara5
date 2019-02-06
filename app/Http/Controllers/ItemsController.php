cc7bdac5340ae3716a9bb61b81fec37dbab02406
He Yang <heyang@Hes-MacBook-Pro.local>
2019-02-06 22:12:01 +1300

    itemapi done

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Item;
+use Validator;
 
 class ItemsController extends Controller
 {
@@ -13,18 +15,11 @@ class ItemsController extends Controller
      */
     public function index()
     {
-        //
+        $items=Item::all();
+        return response()->json($items);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+
 
     /**
      * Store a newly created resource in storage.
@@ -34,7 +29,21 @@ class ItemsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator=Validator::make($request->all(), [
+          'text'=>'required'
+        ]);
+        if($validator->fails()){
+          $response = array('response'=>$validator->messages(), 'success'=>false);
+          return $response;
+        }else{
+          //create Item
+          $item = new Item;
+          $item->text=$request->input('text');
+          $item->body=$request->input('body');
+          $item->save();
+
+          return response()->json($item);
+        }
     }
 
     /**
@@ -45,19 +54,10 @@ class ItemsController extends Controller
      */
     public function show($id)
     {
-        //
+        $item=Item::find($id);
+        return response()->json($item);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
@@ -68,7 +68,21 @@ class ItemsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $validator=Validator::make($request->all(), [
+          'text'=>'required'
+        ]);
+        if($validator->fails()){
+          $response = array('response'=>$validator->messages(), 'success'=>false);
+          return $response;
+        }else{
+          //find an Item
+          $item =Item::find($id);
+          $item->text=$request->input('text');
+          $item->body=$request->input('body');
+          $item->save();
+
+          return response()->json($item);
+        }
     }
 
     /**
@@ -79,6 +93,9 @@ class ItemsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $item =Item::find($id);
+        $item->delete();
+        $response = array('response'=>'Item deleted', 'success'=>true);
+        return $response;
     }
 }
